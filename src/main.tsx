bfd43d2c5e166798e9c51b3a0e28b699815348d7
Beatriz Beraldo <biaberaldo@outlook.com>
2022-06-22 20:38:20 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client' //ReactDOM adaptação do react para web
+import App from './App'
+import './index.css'
+
+// getElementById('root') -> esta no index.html
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
