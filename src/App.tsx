844dfb99ea9c9d064f6cc227f5a6beb90939276a
Beatriz <biaberaldo@outlook.com>
2022-06-25 22:49:58 -0300

    Lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Router } from "./Router";
+import { client } from './lib/apollo';
+import { ApolloProvider } from '@apollo/client';
+
 import Event from "./pages/Event";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
 
 	return (
-		<div>
-			<Event/>
-		</div>	
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</ApolloProvider>
+		
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
-import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client'; //ReactDOM adaptação do react para web
 import App from './App';
-import { client } from './lib/apollo';
 
 import './styles/global.css';
 
 // getElementById('root') -> esta no index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<ApolloProvider client={client}>
-			<App />
-		</ApolloProvider>
+		
+		<App />
+		
 	</React.StrictMode>
 );
